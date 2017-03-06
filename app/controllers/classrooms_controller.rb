0b5c83c237af2ffb5de12dc005d01a3bcd04f660
yellowpad <zayneabraham1@gmail.com>
2017-03-06 16:24:54 -0500

    done

--- app/controllers/classrooms_controller.rb
+++ app/controllers/classrooms_controller.rb
@@ -1,9 +1,8 @@
 class ClassroomsController < ApplicationController
   def show
     @classroom = Classroom.find(params[:id])
-    @student = @classroom.oldest_student
   end
-
+  
   def index
     @classrooms = Classroom.all
   end
